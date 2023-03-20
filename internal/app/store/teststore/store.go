0e7bc93ca5b8fe1466c81b920d77d9f453748056
Eldos02 <91348024+Bexultan200103484@users.noreply.github.com>
2023-03-20 23:23:00 +0600

    Add files via upload

---
+++ internal/app/store/teststore/store.go
@@ -0,0 +1,30 @@
+package teststore
+
+import (
+	"github.com/Bexultan200103484/Midterm1/internal/app/model"
+	"github.com/Bexultan200103484/Midterm1/internal/app/store"
+)
+
+// Store ...
+type Store struct {
+	userRepository *UserRepository
+}
+
+// New ...
+func New() *Store {
+	return &Store{}
+}
+
+// User ...
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+		users: make(map[int]*model.User),
+	}
+
+	return s.userRepository
+}
